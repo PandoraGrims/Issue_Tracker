7c209cc4c6470a75d0bd0ecd4efb38113817abd3
Pandora <nimfanefor@gmail.com>
2023-12-14 19:15:10 +0600

    Добавил модели + новые файлы для работы и сделал настройку. Произвел миграцию

---
+++ issuetracker/webapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Status, Type, Task
+
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Task)
---
+++ issuetracker/webapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False, verbose_name="Статус")
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False, verbose_name="Тип")
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=300, null=False, blank=False, verbose_name="Заголовок")
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Полное_описание')
+    status = models.ForeignKey('webapp.Status', on_delete=models.CASCADE)
+    type = models.ForeignKey('webapp.Type', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Время изменения', auto_now=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.summary}'
