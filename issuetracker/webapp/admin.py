9bf44679179e892e1bd84ae5a5b866e890c3184f
Pandora <nimfanefor@gmail.com>
2023-12-19 19:35:32 +0600

    Добавил в трекер модель Проект, добавил мета классы ко все моделям, зарегенил в админ модель Проекты. Удалил лишнее с вьюшек. Произвел Миграцию

--- issuetracker/webapp/admin.py
+++ issuetracker/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Status, Type, Task
+from .models import Status, Type, Task, Project
 
+admin.site.register(Project)
 admin.site.register(Status)
 admin.site.register(Type)
 admin.site.register(Task)
--- issuetracker/webapp/models.py
+++ issuetracker/webapp/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Project(models.Model):
+    start_date = models.DateField(verbose_name='Дата начала', default=timezone.now)
+    end_date = models.DateField(verbose_name='Дата окончания', null=True, blank=True)
+    name = models.CharField(max_length=300, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(max_length=300, null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+    class Meta:
+        db_table = 'projects'
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
 
 
 class Status(models.Model):
@@ -7,6 +23,11 @@ class Status(models.Model):
     def __str__(self):
         return f'{self.id}. {self.name}'
 
+    class Meta:
+        db_table = "statuses"
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
 
 class Type(models.Model):
     name = models.CharField(max_length=300, null=False, blank=False, verbose_name="Тип")
@@ -14,14 +35,26 @@ class Type(models.Model):
     def __str__(self):
         return f'{self.id}. {self.name}'
 
+    class Meta:
+        db_table = "types"
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+
 
 class Task(models.Model):
     summary = models.CharField(max_length=300, null=False, blank=False, verbose_name="Заголовок")
-    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Полное_описание')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Полное_описание')
     status = models.ForeignKey('webapp.Status', on_delete=models.CASCADE)
     types = models.ManyToManyField('webapp.Type', related_name='tasks', verbose_name='Типы')
     created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Время изменения', auto_now=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Проект', related_name='tasks',
+                                default=1)
 
     def __str__(self):
         return f'{self.id}. {self.summary}'
+
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
